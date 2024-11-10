f78293323bd0246f6b703a750a71e649f385fd95
Arjun Sahlot <iarjun.sahlot@gmail.com>
2024-11-09 19:56:37 -0600

    feat: api in progress

--- src/api.py
+++ src/api.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
 from flask_restx import Resource, Api
+import os
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from parse import parse_outlines
+from gencourse import generate_problems
+from models import ProblemSet
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,9 +25,43 @@ class GenCourse(Resource):
 
         uploaded_file.save("temp.pdf")
 
+        outlines = parse_outlines("temp.pdf")
 
+        course = {}
+
+        course["course_title"] = uploaded_file.filename
+        course["chapters"] = []
+
+        for chapter_title, chapter_sections in outlines:
+            chapter = {}
+            chapter["chapter_title"] = chapter_title
+            chapter["sections"] = []
+
+            for section_title, section_body in chapter_sections:
+                section = {}
+                section["section_title"] = section_title
+
+                problemset: ProblemSet = generate_problems(section_body, 6, 2)
+                problemset = problemset.model_dump()
+
+                section["mcqs"] = problemset["mcqs"]
+                section["frqs"] = problemset["frqs"]
+
+                chapter["sections"].append(section)
+            
+            course["chapters"].append(chapter)
+        
+        
 
         return {"message": f"File '{uploaded_file.filename}' received successfully"}, 200
 
 if __name__ == '__main__':
+    client = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
+
+    try:
+        client.admin.command('ping')
+        print("You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
     app.run(debug=True)
--- src/gencourse.py
+++ src/gencourse.py
@@ -1,24 +1,12 @@
 from langchain_openai import ChatOpenAI
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from models import ProblemSet
 
-from models import MCQQuestion, FRQQuestion, ProblemSet
-
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
-
-try:
-    client.admin.command('ping')
-    print("You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
 LLM = ChatOpenAI(model="gpt-4o-mini").with_structured_output(ProblemSet)
-PROMPT = "Please generate a problem set of {} problems which contain both MCQ and text questions from the following text:\n\n{}"
+PROMPT = "Please generate a problem set of {} MCQ questions and {} FRQ questions from the following text:\n\n{}"
 
-def generate_problems(text: str, num_problems: int) -> ProblemSet:
-    return LLM(PROMPT.format(num_problems, text))
+def generate_problems(text: str, mcq_count: int, frq_count: int) -> ProblemSet:
+    return LLM(PROMPT.format(mcq_count, frq_count, text))
